6f30b419dcce9afb5dc0d01b17ec84ec67e4cefc
Irete Vincent <centmuede@gmail.com>
2023-03-16 10:20:33 +0100

    first commit

---
+++ SecondWeb/src/main/java/com/mycompany/secondweb/LoginServlet.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.secondweb;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+
+
+public class LoginServlet extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+     
+            String userName  = request.getParameter("username");
+            String passWord  = request.getParameter("password");
+            
+           // out.println(userName);
+           // out.println(passWord);
+            
+            if(userName==null || userName.equals("")&&passWord ==null ||passWord.equals("") ){
+                
+                out.println("Enter your Name or Password!");
+                RequestDispatcher rd = request.getRequestDispatcher("LoginForm.html");
+                rd.include(request, response);
+                
+                
+            }
+            else if(userName.equals("Vincent")&&passWord.equals("admin") ){
+                 RequestDispatcher rd = request.getRequestDispatcher("dashboard.html");
+                rd.include(request, response);
+                   
+            }
+            else{
+            out.println("Enter user Name or Password!!!");
+             RequestDispatcher rd = request.getRequestDispatcher("LoginForm.html");
+             rd.include(request, response);
+                
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
